1f1b511278ee693297c5ec5e0b7d75799ae3d97b
Nick Smith <smithne@uw.edu>
2019-07-26 11:57:21 +0800

    Add logic to switch tab content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import drawHeader from './staticContent';
 
-drawHeader('content');
+const conatinerID = 'content';
+const containerDiv = document.getElementById(conatinerID);
 
+drawHeader(conatinerID);
+
+const sections = ['Contact', 'Menu'];
+
+// create div to contain tabs
+const navBarDiv = document.createElement("div");
+navBarDiv.id = 'navBar';
+
+// add navigation tabs
+for (let i = 0; i < sections.length; i++) {
+    let element = document.createElement("div");
+    element.id = "navBtn-" + sections[i];
+    element.classList.add("navBtn");
+    element.innerHTML = sections[i];
+    element.addEventListener("click", function() {
+        selectSection(sections[i]);
+    });
+    navBarDiv.appendChild(element);
+}
+containerDiv.appendChild(navBarDiv);
+
+// add div for tab content
+const tabContentDiv = document.createElement("div");
+tabContentDiv.id = "tabContent";
+tabContentDiv.innerText = "Placeholder text";
+containerDiv.appendChild(tabContentDiv);
+
+// add logic to switch 
+const selectSection = (section) => {
+    tabContentDiv.innerHTML = `This is the ${section} content.`;
+}
 
